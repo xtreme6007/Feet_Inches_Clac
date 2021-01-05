3e74d961def2c934830308688970a5f6e5b3f7f6
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 11:07:02 -0600

    added method and validate all numbers all valid numbers

--- src/academy/learnprograming/Main.java
+++ src/academy/learnprograming/Main.java
@@ -5,4 +5,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
     }
+
+    public static int clacFeetAndInchesToCentimeters(int feet, int inches){
+        if(feet >= 0 && inches >= 0){
+
+        }
+
+
+
+    }
 }
