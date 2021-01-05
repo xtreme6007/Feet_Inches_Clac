7b47f1bc6f60dc87215acbccd482d880c5e0612c
xtreme6007 <prestonnichols96@gmail.com>
2021-01-05 11:20:51 -0600

    added second overload method and linked both methods together

--- src/academy/learnprograming/Main.java
+++ src/academy/learnprograming/Main.java
@@ -3,15 +3,26 @@ package academy.learnprograming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 
-    public static double clacFeetAndInchesToCentimeters(int feet, int inches){
-        if(feet >= 0 && inches >= 0){
+    public static double clacFeetAndInchesToCentimeters(int feet, int inches) {
+        if (feet >= 0 && (inches >= 0 && inches <= 12)) {
             double CentFt = feet * 30.48;
             double CentIn = inches * 2.54;
-            return (double)(CentFt + CentIn);
+            return (double) (CentFt + CentIn);
         }
+        return -1;
+    }
+        public static double clacFeetAndInchesToCentimeters(int inches){
+            if(inches >= 0){
+
+                int feet = inches / 12;
+                int remainder = inches % 12;
+                System.out.println(clacFeetAndInchesToCentimeters(feet, remainder));
+
+            }
+            return -1;
 
 
 
